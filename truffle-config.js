9f04ea900442f6fc3c1418e807d804a34cf912ea
dinh hoang nam <dinhhoangnam998@gmail.com>
2020-03-31 12:00:06 +0700

    move key to .env

--- truffle-config.js
+++ truffle-config.js
@@ -1,19 +1,14 @@
+require('dotenv').config()
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-
-// TODO: move to .env
-const localPrivateKey = '42e6b866ddfd14d8d9d98999315ef9483d9ce65f25a0f2d108d8255518dfd3ca';
-const privateKey = '6A6CFFD6BED34FB3BB6EEBD27F6786690912EAF9FC75B1CD28533BAB951D059C';
-const localProvider = 'HTTP://127.0.0.1:8545';
-const provider = `https://ropsten.infura.io/v3/fb1c2f2f566443d08c59aae698c78839`;
 
 module.exports = {
   networks: {
     development: {
-      provider: () => new HDWalletProvider(localPrivateKey, localProvider),
-      network_id: "*",       // Any network (default: none)
+      provider: () => new HDWalletProvider(process.env.LOCAL_PRIVATE_KEY, process.env.LOCAL_PROVIDER),
+      network_id: "*",
     },
     ropsten: {
-      provider: () => new HDWalletProvider(privateKey, provider),
+      provider: () => new HDWalletProvider(process.env.ROPSTEN_PRIVATE_KEY, process.env.ROPSTEN_PROVIDER),
       network_id: 3,
     },
   },
